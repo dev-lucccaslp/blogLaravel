820a190dcbe06ec4f4bdd7d4b92d030e63d53b90
Lucas Lessa Pacheco <lucas.pacheco@box360.net.br>
2022-12-13 22:53:41 -0300

    crate migrantions and updated in DB

---
+++ app/Models/Posts.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Posts extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'status',
+        'user_id',
+    ];
+//CONFIGURANDO RELACIONAMENTO DO POST COM USER PARA RECEBER O ID DO USERS
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2022_12_14_005034_create_posts.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 45);
+            $table->text('content', 200);
+            $table->enum('status', ['publicado', 'rascunho'])->default('rascunho');
+            $table->foreignIdFor(User::class)->references('id')->on('users')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function(Blueprint $table) {
+            $table->dropForeignIdFor(User::class);
+        });
+        Schema::dropIfExists('posts');
+    }
+};
